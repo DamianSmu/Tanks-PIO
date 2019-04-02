ec57670854c735a187c8d8de27cc9358b24536e4
mecinas <szymon.mecina@vp.pl>
2019-04-02 19:57:41 +0200

    Added Background class

---
+++ core/src/com/pio/tanks/Background.java
@@ -0,0 +1,36 @@
+package com.pio.tanks;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Background extends Actor
+{
+    private TextureRegion texture;
+
+    public Background(Stage stage)
+    {
+        super();
+        stage.addActor(this);
+        texture = new TextureRegion(new Texture("uncolored_hills.png"));
+        setSize(1024, 1024);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+        Color c = getColor();
+        batch.setColor(c.r, c.g, c.b, c.a);
+
+        if (isVisible())
+        {
+            batch.draw(texture,
+                    getX(), getY(), getOriginX(), getOriginY(),
+                    getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+        }
+
+    }
+}
