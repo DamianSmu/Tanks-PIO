04727ccd056cfe098c1e961150e6e0ccc72a0faf
mecinas <szymon.mecina@vp.pl>
2019-05-28 20:28:45 +0200

    Dodanie kolejnych testów

--- Tests/src/com/pio/tests/Tests.java
+++ Tests/src/com/pio/tests/Tests.java
@@ -3,6 +3,7 @@ package com.pio.tests;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.pio.tanks.BackgroundActors.Cloud;
+import com.pio.tanks.BackgroundActors.Tower;
 import com.pio.tanks.BackgroundActors.Tree;
 import com.pio.tanks.BackgroundManager;
 import com.pio.tanks.Tank;
@@ -15,12 +16,24 @@ import static org.junit.Assert.assertTrue;
 
 
 @RunWith(GdxTestRunner.class)
-public class Example
+public class Tests
 {
     @Test
-    public void test()
+    public void treePictureTest()
     {
         assertTrue("Nie działa", Gdx.files.local("..\\core\\assets\\tree02.png").exists());
+    }
+
+    @Test
+    public void cloudPictureTest()
+    {
+        assertTrue("Nie działa", Gdx.files.local("..\\core\\assets\\cloud1.png").exists());
+    }
+
+    @Test
+    public void bulletPictureTest()
+    {
+        assertTrue("Nie działa", Gdx.files.local("..\\core\\assets\\tank_bullet3.png").exists());
     }
 
     @Test
@@ -33,16 +46,27 @@ public class Example
     @Test
     public void treeTest()
     {
-        Tree t = new Tree(100,100);
+        Tree t = new Tree(100,200);
         float posX = t.getX();
-        float getY = t.getY();
-        assertTrue(true);
+        assertEquals(150, posX, 50);
+
     }
 
     @Test
     public void cloudTest()
     {
-        Cloud c = new Cloud(100,100);
-        assertTrue(true);
+        Cloud c = new Cloud(100,200);
+        float posX = c.getX();
+        assertEquals(150, posX, 50);
+    }
+
+    @Test
+    public void towerTest(){
+        Tower t =  new Tower(100,500);
+        float width = t.getWidth();
+        float height = t.getHeight();
+
+        assertEquals(100, width, 0);
+        assertEquals(500, height, 0);
     }
 }
--- core/src/com/pio/tanks/BackgroundActors/Tree.java
+++ core/src/com/pio/tanks/BackgroundActors/Tree.java
@@ -113,4 +113,6 @@ public class Tree extends Actor
             }
         }
     }
+
+
 }
